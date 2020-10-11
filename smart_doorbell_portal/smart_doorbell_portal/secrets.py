5a1b135a3d45a70b11d13bb3ca1436f21eaddc6e
cd3vane <devane.charles98@gmail.com>
2020-10-11 16:14:50 -0400

    Users can now register, working on login next

--- smart_doorbell_portal/smart_doorbell_portal/secrets.py
+++
@@ -1,2 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e$i*7y84ddr)_vn02581!d-$)-^cduijxa4%myv)o(k_a7&mjv'
--- smart_doorbell_portal/smart_doorbell_portal/settings.py
+++ smart_doorbell_portal/smart_doorbell_portal/settings.py
@@ -16,13 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_KEY", "218974y9qdhfusaohf23789489dhfu24y8")
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
