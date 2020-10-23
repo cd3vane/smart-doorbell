78d067ffe859c027287e25da4d6530ae7933d12c
cd3vane <devane.charles98@gmail.com>
2020-10-23 00:54:36 -0400

    working on user profile and device registration

--- smart_doorbell_portal/device/models.py
+++ smart_doorbell_portal/device/models.py
@@ -1,7 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Device(models.Model):
+    user = User.username
     device_id = models.CharField(max_length=50)
+
+    def is_connected(self):
+        if self.user == '':
+            return False
+        return True
