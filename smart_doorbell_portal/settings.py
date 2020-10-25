0d2436e1c5b3d6b0df098bada4c2f42fcdd226b1
cd3vane <devane.charles98@gmail.com>
2020-10-25 16:58:35 -0400

    folders reworked

--- smart_doorbell_portal/settings.py
+++ smart_doorbell_portal/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get("DJANGO_KEY", "218974y9qdhfusaohf23789489dhfu24y8")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
