7e116be81670d0fd96d6e7419918545b924ca525
cd3vane <devane.charles98@gmail.com>
2020-10-26 14:11:07 -0400

    first name and last name fields are not saving values

--- users/forms.py
+++ users/forms.py
@@ -12,7 +12,7 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = [
-            'username', 'email', 'password1', 'password2'
+            'username', 'email', 'first_name', 'last_name', 'password1', 'password2'
             ]
 
 
