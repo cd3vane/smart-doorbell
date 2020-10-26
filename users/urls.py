152d4a5bee612a0d630a8ddadc655419a5fef1c9
cd3vane <devane.charles98@gmail.com>
2020-10-26 13:51:53 -0400

    Profile updating is now functional with profile pictures

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import register_view, profile_view
+from .views import register_view, profile_view, profile_update_view
 
 
 urlpatterns = [
     path('register/',  register_view, name='register'),
     path('account/profile', profile_view, name='profile'),
+    path('account/update/profile', profile_update_view, name='update_profile'),
     ]
--- users/views.py
+++ users/views.py
@@ -18,13 +18,30 @@ def register_view(request):
     return render(request, 'users/register.html', {'form': form})
 
 
-
 @login_required
 def profile_view(request):
-    u_form = UserUpdateForm(instance=request.user)
-    p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    return render(request, 'users/profile.html')
+
+
+@login_required
+def profile_update_view(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your profile has been successfully updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
     context = {
         'u_form': u_form,
         'p_form': p_form
     }
-    return render(request, 'users/profile.html', context)
+    return render(request, 'users/profile_update.html', context)
