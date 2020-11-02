e9fd2669dd25570787422210eab073b2d8a3d7a6
cd3vane <devane.charles98@gmail.com>
2020-11-01 20:03:27 -0500

    updated url patterns

--- smart_doorbell_portal/urls.py
+++ smart_doorbell_portal/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
@@ -10,8 +9,6 @@ urlpatterns = [
     path('', include('users.urls')),
     path('devices/', include('device.urls')),
     path('events/', include('events.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
 ]
 
 if settings.DEBUG is True:
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from .views import register_view, profile_view, profile_update_view
 
@@ -6,5 +7,7 @@ from .views import register_view, profile_view, profile_update_view
 urlpatterns = [
     path('register/',  register_view, name='register'),
     path('account/profile', profile_view, name='profile'),
-    path('account/update/profile', profile_update_view, name='update_profile'),
+    path('account/update-profile', profile_update_view, name='update_profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     ]
