84e9841ba8ed29c11171b9d299663799c6c430c5
cd3vane <devane.charles98@gmail.com>
2020-11-23 11:58:15 -0500

    fixed event table

--- events/models.py
+++ events/models.py
@@ -2,13 +2,11 @@ from django.db import models
 from datetime import datetime
 import string
 import random
-
 # Create your models here.
 
 
 class Event(models.Model):
-    device_id = models.IntegerField(max_length=50)
-    event_id = models.IntegerField(max_length=20)
+    device_id = models.IntegerField(default=1)
     event_type = models.CharField(max_length=10)
 
    
